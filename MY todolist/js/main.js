db385b280e299ba417230199115e7559433d5c22
JWX <774578034@qq.com>
2019-06-25 20:13:25 +0800

    完成部分功能

---
+++ MY todolist/js/main.js
@@ -0,0 +1,72 @@
+function copy(obj) {
+    return Object.assign({}, obj);
+}
+new Vue({
+    el: '#main',
+    data: {
+        list: [],
+        current: {
+            title: '',
+            completed: false,
+            desc: '',
+            remind_at: '2020-10-01'
+        }
+    },
+    mounted: function() {
+        this.list = ms.get('list') || this.list;
+    },
+    methods: {
+        merge: function() {
+            var is_update, id;
+            is_update = id = this.current.id;
+            if (is_update) {
+                var index = this.find_index(id);
+                Vue.set(this.list, index, copy(this.current));
+
+                this.reset_current();
+            } else {
+                var title = this.current.title;
+                if (!title && title !== 0) return
+                var todo = copy(this.current);
+                todo.id = this.next_id();
+                this.list.push(todo);
+                this.reset_current();
+            }
+
+        },
+
+        remove: function(id) {
+            var index = this.find_index(id);
+            this.list.splice(index, 1);
+
+        },
+        next_id: function() {
+            return this.list.length + 1;
+        },
+        set_current: function(todo) {
+            this.current = copy(todo);
+        },
+        reset_current: function() {
+            this.set_current({});
+        },
+        find_index: function(id) {
+            return this.list.findIndex(function(item) {
+                return item.id == id;
+            })
+        }
+    },
+    watch: {
+        list: {
+            deep: true,
+            handler: function(n, o) {
+                if (n) {
+                    ms.set('list', n);
+
+                } else {
+                    ms.set('list', []);
+                }
+            }
+
+        }
+    }
+})
