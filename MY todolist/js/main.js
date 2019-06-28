dfb94d15b92e037809a0366bbb21ffc7a545105a
JWX <774578034@qq.com>
2019-06-28 17:28:55 +0800

    实现功能+页面样式

--- MY todolist/js/main.js
+++ MY todolist/js/main.js
@@ -1,72 +1,173 @@
-function copy(obj) {
-    return Object.assign({}, obj);
-}
-new Vue({
-    el: '#main',
-    data: {
-        list: [],
-        current: {
-            title: '',
-            completed: false,
-            desc: '',
-            remind_at: '2020-10-01'
-        }
-    },
-    mounted: function() {
-        this.list = ms.get('list') || this.list;
-    },
-    methods: {
-        merge: function() {
-            var is_update, id;
-            is_update = id = this.current.id;
-            if (is_update) {
-                var index = this.find_index(id);
-                Vue.set(this.list, index, copy(this.current));
+/* myStorage.js */ ;
+(function() {
+    window.ms = {
+        set: set,
+        get: get,
+    };
 
-                this.reset_current();
-            } else {
-                var title = this.current.title;
-                if (!title && title !== 0) return
-                var todo = copy(this.current);
-                todo.id = this.next_id();
-                this.list.push(todo);
-                this.reset_current();
+    function set(key, val) {
+        localStorage.setItem(key, JSON.stringify(val));
+    }
+
+    function get(key) {
+        var json = localStorage.getItem(key);
+        if (json) {
+            return JSON.parse(json);
+        }
+    }
+})();
+
+/* main.js */
+;
+(function() {
+    'use strict';
+
+    var Event = new Vue();
+
+    var alert_sound = document.getElementById('alert-sound');
+
+    function copy(obj) {
+        return Object.assign({}, obj);
+    }
+
+    Vue.component('task', {
+        template: '#task-tpl',
+        props: ['todo'],
+        methods: {
+            action: function(name, params) {
+                Event.$emit(name, params);
             }
-
-        },
-
-        remove: function(id) {
-            var index = this.find_index(id);
-            this.list.splice(index, 1);
-
-        },
-        next_id: function() {
-            return this.list.length + 1;
-        },
-        set_current: function(todo) {
-            this.current = copy(todo);
-        },
-        reset_current: function() {
-            this.set_current({});
-        },
-        find_index: function(id) {
-            return this.list.findIndex(function(item) {
-                return item.id == id;
-            })
         }
-    },
-    watch: {
-        list: {
-            deep: true,
-            handler: function(n, o) {
-                if (n) {
-                    ms.set('list', n);
+    })
 
+    new Vue({
+        el: '#main',
+        data: {
+            list: [],
+            last_id: 0,
+            current: {}
+        },
+
+        mounted: function() {
+            var me = this;
+            this.list = ms.get('list') || this.list;
+            this.last_id = ms.get('last_id') || this.last_id;
+
+            setInterval(function() {
+                me.check_alerts();
+            }, 1000);
+
+            Event.$on('remove', function(id) {
+                if (id) {
+                    me.remove(id);
+                }
+            });
+
+            Event.$on('toggle_complete', function(id) {
+                if (id) {
+                    me.toggle_complete(id);
+                }
+            });
+
+            Event.$on('set_current', function(id) {
+                if (id) {
+                    me.set_current(id);
+                }
+            });
+
+            Event.$on('toggle_detail', function(id) {
+                if (id) {
+                    me.toggle_detail(id);
+                }
+            });
+        },
+
+        methods: {
+            check_alerts: function() {
+                var me = this;
+                this.list.forEach(function(row, i) {
+                    var alert_at = row.alert_at;
+                    if (!alert_at || row.alert_confirmed) return;
+
+                    var alert_at = (new Date(alert_at)).getTime();
+                    var now = (new Date()).getTime();
+
+                    if (now >= alert_at) {
+                        alert_sound.play();
+                        var confirmed = confirm(row.title);
+                        Vue.set(me.list[i], 'alert_confirmed', confirmed);
+                    }
+                })
+            },
+
+            merge: function() {
+                var is_update, id;
+                is_update = id = this.current.id;
+
+                if (is_update) {
+                    var index = this.find_index(id);
+                    Vue.set(this.list, index, copy(this.current));
                 } else {
-                    ms.set('list', []);
+                    var title = this.current.title;
+                    if (!title && title !== 0) return;
+
+                    var todo = copy(this.current);
+                    this.last_id++;
+                    ms.set('last_id', this.last_id);
+                    todo.id = this.last_id;
+                    this.list.push(todo);
+                }
+
+                this.reset_current();
+            },
+
+            toggle_detail: function(id) {
+                var index = this.find_index(id);
+                Vue.set(this.list[index], 'show_detail', !this.list[index].show_detail)
+            },
+
+            remove: function(id) {
+                var index = this.find_index(id);
+                this.list.splice(index, 1);
+            },
+
+            next_id: function() {
+                return this.list.length + 1;
+            },
+
+            set_current: function(todo) {
+                this.current = copy(todo);
+            },
+
+            reset_current: function() {
+                this.set_current({});
+            },
+
+            find_index: function(id) {
+                return this.list.findIndex(function(item) {
+                    return item.id == id;
+                })
+            },
+
+            toggle_complete: function(id) {
+                var i = this.find_index(id);
+                Vue.set(this.list[i], 'completed', !this.list[i].completed);
+            }
+        },
+
+        watch: {
+            list: {
+                deep: true,
+                handler: function(n, o) {
+                    if (n) {
+                        ms.set('list', n);
+                    } else {
+                        ms.set('list', []);
+                    }
                 }
             }
 
         }
-    }
-})
+    });
+
+})();
